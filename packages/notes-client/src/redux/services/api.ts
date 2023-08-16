1a619c743d6c48fd1806b0ca3aae46c46b5b569f
svetlana tselikova <svetacelikova1@gmail.com>
2023-08-16 17:28:55 +0400

    add docker compose for apps

--- packages/notes-client/src/redux/services/api.ts
+++ packages/notes-client/src/redux/services/api.ts
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { API_GATE_URL } from '@self-notes/utils';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3000',
+    baseUrl: API_GATE_URL,
     credentials: 'include',
   }),
   endpoints: () => ({}),
-  tagTypes: ['Notes']
+  tagTypes: ['Notes'],
 });
