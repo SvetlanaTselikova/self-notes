57d680dd4f888500db295268f987bce24129332a
svetlana tselikova <svetacelikova1@gmail.com>
2023-08-23 15:13:55 +0400

    fix build

--- packages/core-client/webpack.prod.config.js
+++ packages/core-client/webpack.prod.config.js
@@ -2,5 +2,5 @@ const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
-  remotes: [['notes-client', 'https://localhost:4201']],
+  remotes: [['notes-client', 'http://localhost:4201/remoteEntry.js']],
 });
